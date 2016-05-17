551296d530e3e969aa103a02d6dabe1a24fcdd2f
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-05-17 20:59:30 +0100

    Simple start with heading for app

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,5 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import Main from './Components/Main';
+import css from './styles/style.styl';
+render(<Main/>, document.getElementById('root'));
